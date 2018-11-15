f7c79b5cb50e06ec429f3be63bf8b670112f91b5
IsumiAlice <a1261492496@outlook.com>
2018-11-15 16:26:45 +0800

    'abeautifulGUI'

--- PyQt-test/main.py
+++ PyQt-test/main.py
@@ -9,9 +9,36 @@ import qtawesome
     垂直排列布局：QVBoxLayout
 每种布局都有自己对布局内小部件的控制方式和特点，在此我们选择网格布局
 '''
-
+button_style_red = '''
+QPushButton{
+    background: #F76677;
+    border-radius: 5px;
+}
+QpushButton: hover{
+    background: red;
+}
+'''
+button_style_yellow = '''
+QPushButton{
+    background: #F7D674;
+    border-radius: 5px;
+}
+QpushButton: hover{
+    background: yellow;
+}
+'''
+button_style_green = '''
+QPushButton{
+    background: #6DDF6D;
+    border-radius: 5px;
+}
+QpushButton: hover{
+    background: green;
+}
+'''
 
 class MainUI(QtWidgets.QMainWindow):
+    
     def __init__(self):
         super().__init__()
 
@@ -102,11 +129,260 @@ class MainUI(QtWidgets.QMainWindow):
         self.right_layout.addWidget(self.right_bar_widget, 0, 0, 1, 9)
 
         # 今日推荐
+        # 在推荐音乐模块中，有一个推荐的标题，和一个横向排列的音乐封面列表，在这里：
+        # 推荐标题使用QLable()来实现；
+        # 音乐封面列表由多个QToolButton()组成，其继续由一个布局为QGridLayout()的QWidget()部件所包含。
+        self.right_recommend_label = QtWidgets.QLabel('recommend')
+        self.right_recommend_label.setObjectName('right_label')
 
-        # 最新歌曲  /tab    热门歌单
+        self.right_recommend_widget = QtWidgets.QWidget()
+        self.right_recommend_layout = QtWidgets.QGridLayout()
+        self.right_recommend_widget.setLayout(self.right_recommend_layout)
+
+        self.recommend_button_1 = QtWidgets.QToolButton()
+        self.recommend_button_1.setText('可馨HANM')
+        self.recommend_button_1.setIcon(QtGui.QIcon('cover/r1.jpg'))
+        self.recommend_button_1.setIconSize(QtCore.QSize(100, 100))
+        self.recommend_button_1.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)  # 设置按钮形式为上图下文
+
+        self.recommend_button_2 = QtWidgets.QToolButton()
+        self.recommend_button_2.setText('那首歌')
+        self.recommend_button_2.setIcon(QtGui.QIcon('cover/r2.jpg'))
+        self.recommend_button_2.setIconSize(QtCore.QSize(100, 100))
+        self.recommend_button_2.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)  # 设置按钮形式为上图下文
+        
+        self.recommend_button_3 = QtWidgets.QToolButton()
+        self.recommend_button_3.setText('伟大的渺小')
+        self.recommend_button_3.setIcon(QtGui.QIcon('cover/r3.jpg'))
+        self.recommend_button_3.setIconSize(QtCore.QSize(100, 100))
+        self.recommend_button_3.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)  # 设置按钮形式为上图下文
+        
+        self.recommend_button_4 = QtWidgets.QToolButton()
+        self.recommend_button_4.setText('荣耀征战')
+        self.recommend_button_4.setIcon(QtGui.QIcon('cover/r4.jpg'))
+        self.recommend_button_4.setIconSize(QtCore.QSize(100, 100))
+        self.recommend_button_4.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)  # 设置按钮形式为上图下文
+
+        self.recommend_button_5 = QtWidgets.QToolButton()
+        self.recommend_button_5.setText('猎场合辑')
+        self.recommend_button_5.setIcon(QtGui.QIcon('cover/r5.jpg'))
+        self.recommend_button_5.setIconSize(QtCore.QSize(100, 100))
+        self.recommend_button_5.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)  # 设置按钮形式为上图下文
+
+        self.right_recommend_layout.addWidget(self.recommend_button_1, 0, 0)
+        self.right_recommend_layout.addWidget(self.recommend_button_2, 0, 1)
+        self.right_recommend_layout.addWidget(self.recommend_button_3, 0, 2)
+        self.right_recommend_layout.addWidget(self.recommend_button_4, 0, 3)
+        self.right_recommend_layout.addWidget(self.recommend_button_5, 0, 4)
+
+        self.right_layout.addWidget(self.right_recommend_label, 1, 0 ,1, 9)
+        self.right_layout.addWidget(self.right_recommend_widget, 2, 0, 2, 9)
+
+        # 歌曲列表  /tab    热门歌单
+        # 音乐列表模块和音乐歌单模块都有一个标题和一个小部件来容纳具体的内容
+        # 其中标题我们都使用QLabel()部件来实现，而音乐列表我们使用网格布局的QWidget()部件下包裹着数个QPushButton()按钮部件来实现
+        # 音乐歌单列表则使用网格布局的QWidget()部件下包裹着数个QToolButton()工具按钮部件来实现
+        self.right_newsong_label = QtWidgets.QLabel('new www_www')
+        self.right_newsong_label.setObjectName('right_label')
+
+        self.right_newsong_widget = QtWidgets.QWidget()
+        self.right_newsong_layout = QtWidgets.QGridLayout()
+        self.right_newsong_widget.setLayout(self.right_newsong_layout)
+
+        self.newsong_button_1 = QtWidgets.QPushButton('BBButtton1')
+        self.newsong_button_2 = QtWidgets.QPushButton('BBButtton2')
+        self.newsong_button_3 = QtWidgets.QPushButton('BBButtton3')
+        self.newsong_button_4 = QtWidgets.QPushButton('BBButtton4')
+        self.newsong_button_5 = QtWidgets.QPushButton('BBButtton5')
+        self.newsong_button_6 = QtWidgets.QPushButton('BBButtton6')
+        
+        self.right_newsong_layout.addWidget(self.newsong_button_1, 0, 1)
+        self.right_newsong_layout.addWidget(self.newsong_button_2, 1, 1)
+        self.right_newsong_layout.addWidget(self.newsong_button_3, 2, 1)
+        self.right_newsong_layout.addWidget(self.newsong_button_4, 3, 1)
+        self.right_newsong_layout.addWidget(self.newsong_button_5, 4, 1)
+        self.right_newsong_layout.addWidget(self.newsong_button_6, 5, 1)
+
+        self.right_playlist_label = QtWidgets.QLabel('MMMM_MMMM')
+        self.right_playlist_label.setObjectName('right_label')
+
+        self.right_playlist_widget = QtWidgets.QWidget()
+        self.right_playlist_layout = QtWidgets.QGridLayout()
+        self.right_playlist_widget.setLayout(self.right_playlist_layout)
+
+        self.playlist_button_1 = QtWidgets.QToolButton()
+        self.playlist_button_1.setText('name1')
+        self.playlist_button_1.setIcon(QtGui.QIcon('cover/p1.jpg'))
+        self.playlist_button_1.setIconSize(QtCore.QSize(100, 100))
+        self.playlist_button_1.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+
+        self.playlist_button_2 = QtWidgets.QToolButton()
+        self.playlist_button_2.setText('name2')
+        self.playlist_button_2.setIcon(QtGui.QIcon('cover/p2.jpg'))
+        self.playlist_button_2.setIconSize(QtCore.QSize(100, 100))
+        self.playlist_button_2.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+
+        self.playlist_button_3 = QtWidgets.QToolButton()
+        self.playlist_button_3.setText('name3')
+        self.playlist_button_3.setIcon(QtGui.QIcon('cover/p3.jpg'))
+        self.playlist_button_3.setIconSize(QtCore.QSize(100, 100))
+        self.playlist_button_3.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+
+        self.playlist_button_4 = QtWidgets.QToolButton()
+        self.playlist_button_4.setText('name4')
+        self.playlist_button_4.setIcon(QtGui.QIcon('cover/p4.jpg'))
+        self.playlist_button_4.setIconSize(QtCore.QSize(100, 100))
+        self.playlist_button_4.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+
+        self.right_playlist_layout.addWidget(self.playlist_button_1, 0, 0)
+        self.right_playlist_layout.addWidget(self.playlist_button_2, 0, 1)
+        self.right_playlist_layout.addWidget(self.playlist_button_3, 1, 0)
+        self.right_playlist_layout.addWidget(self.playlist_button_4, 1, 1)
+
+        # 上面两部分添加到右侧布局中
+        self.right_layout.addWidget(self.right_newsong_label, 4, 0, 1, 5)
+        self.right_layout.addWidget(self.right_playlist_label, 4, 5, 1, 4)
+        self.right_layout.addWidget(self.right_newsong_widget, 5, 0, 1, 5)
+        self.right_layout.addWidget(self.right_playlist_widget, 5, 5, 1, 4)
+
         # 播放条
-#        self.init_ui()
+        # 音乐播放进度条我们使用QProgressBar()进度条部件来实现
+        self.right_process_bar = QtWidgets.QProgressBar()  # 播放进度
+        self.right_process_bar.setValue(49)
+        self.right_process_bar.setFixedHeight(3)
+        self.right_process_bar.setTextVisible(False)  # 不显示文字
 
+        # 音乐播放控制按钮组则使用一个QWidget()部件下包裹着三个QPushButton()按钮部件来实现
+        self.right_playconsole_widget = QtWidgets.QWidget()
+        self.right_playconsole_layout = QtWidgets.QGridLayout()
+        self.right_playconsole_widget.setLayout(self.right_playconsole_layout)
+
+        self.console_button_1 = QtWidgets.QPushButton(qtawesome.icon('fa.backward', color = '#F76677'), '')
+        self.console_button_2 = QtWidgets.QPushButton(qtawesome.icon('fa.forward', color = '#F76677'), '')
+        self.console_button_3 = QtWidgets.QPushButton(qtawesome.icon('fa.pause', color = '#F76677'), '')
+        self.console_button_3.setIconSize(QtCore.QSize(30, 30))
+
+        self.right_playconsole_layout.addWidget(self.console_button_1, 0, 0)
+        self.right_playconsole_layout.addWidget(self.console_button_2, 0, 2)
+        self.right_playconsole_layout.addWidget(self.console_button_3, 0, 1)
+        self.right_playconsole_layout.setAlignment(QtCore.Qt.AlignCenter)  # 设置布局内部件居中显示
+        
+        self.right_layout.addWidget(self.right_process_bar, 9, 0, 1, 9)
+        self.right_layout.addWidget(self.right_playconsole_widget, 10, 0, 1, 9)
+
+
+        self.fun_QSS()
+        self.fun_QT5()
+    
+    # 美化
+    def fun_QSS(self):
+        # 左上 三个按钮
+        self.left_close.setFixedSize(15, 15)
+        self.left_visit.setFixedSize(15, 15)
+        self.left_mini.setFixedSize(15, 15)
+
+        self.left_close.setStyleSheet(button_style_red)
+        self.left_visit.setStyleSheet(button_style_yellow)
+        self.left_mini.setStyleSheet(button_style_green)
+
+
+        # 将左侧菜单中的按钮和文字颜色设置为白色，并将按钮的边框去掉
+        self.left_widget.setStyleSheet('''
+            QWidget#left_widget{
+                background:darkGray;
+                border-top:1px solid darkGray;
+                border-bottom:1px solid darkGray;
+                border-right:1px solid darkGray;
+                border-top-left-radius:10px;
+                border-bottom-left-radius:10px;
+            }
+            QPushButton{border:none;color:white;}
+            QPushButton#left_label{
+                border:none;
+                border-bottom:1px solid white;
+                font-size:18px;
+                font-weight:700;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            }
+            QPushButton#left_button:hover{border-left:4px solid red;font-weight:700;}
+        ''')
+
+        # 右侧搜索框 圆角
+        self.right_bar_widget_search_input.setStyleSheet('''
+            QLineEdit{
+                border:1px solid gray;
+                width:300px;
+                border-radius:10px;
+                padding:2px 4px;
+            }
+        ''')
+
+        # 右侧部件 右上角和右下角圆角 背景设置为白色
+        # 字体放大
+        self.right_widget.setStyleSheet('''
+            QWidget#right_widget{
+                color:#232C51;
+                background:white;
+                border-top:1px solid darkGray;
+                border-bottom:1px solid darkGray;
+                border-right:1px solid darkGray;
+                border-top-right-radius:10px;
+                border-bottom-right-radius:10px;
+            }
+
+            QLabel#right_label{
+                border:none;
+                font-size:16px;
+                font-weight:700;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            }
+        ''')
+
+        # 推荐模块和歌单模块 QToolButton
+        self.right_recommend_widget.setStyleSheet('''
+            QToolButton{border:none;}
+            QToolButton:hover{border-bottom:2px solid #F76677;}
+        ''')
+        self.right_playlist_widget.setStyleSheet('''
+            QToolButton{border:none;}
+            QToolButton:hover{border-bottom:2px solid #F76677;}
+        ''')
+        
+        # 音乐列表使用的是QPushButton()按钮部件，我们需要对其去除边框，修改字体和颜色等
+        self.right_newsong_widget.setStyleSheet('''
+            QPushButton{
+                border:none;
+                color:gray;
+                font-size:12px;
+                height:40px;
+                padding-left:5px;
+                padding-right:10px;
+                text-align:left;
+            }
+            QPushButton:hover{
+                color:black;
+                border:1px solid #F3F3F5;
+                border-radius:10px;
+                background:LightGray;
+            }
+        ''')
+
+        # 将播放进度条的样色设置为浅红色，然后去除播放控制按钮的边框
+        self.right_process_bar.setStyleSheet('''
+            QProgressBar::chunk{background-color: #F76677;}
+        ''')
+        self.right_playconsole_widget.setStyleSheet('''
+            QPushButton{border:none;}
+        ''')
+
+    def fun_QT5(self):
+        self.setWindowOpacity(0.9)  # 透明度
+        
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 窗口背景透明
+
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
+
+        self.main_layout.setSpacing(0)  # 无缝
 #    def init_ui(self):
 
 def main():
