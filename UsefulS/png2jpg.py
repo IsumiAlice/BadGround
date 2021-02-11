985368ceff2872041f5efbea608b30194c404d85
IsumiAlice <isumialice@foxmail.com>
2021-02-11 20:27:29 +0800

    CV->PIL

--- UsefulS/png2jpg.py
+++ UsefulS/png2jpg.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-def jpg2png(path):
+def png2jpg(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             filepath = os.path.join(root, file)
@@ -9,13 +9,9 @@ def jpg2png(path):
             if fileext == ('.png' or '.PNG'):
                 img = Image.open(filepath).convert("RGB")
                 img.save(filepath.replace('png', 'jpg'))
-                # src = cv2.imread(filepath)
                 print(filepath.replace('png', 'jpg'))
-                # cv2.imwrite(filepath.replace('png', 'jpg'), src)
-                # cv2.imwrite('1.jpg', src)
                 os.remove(filepath)
-
 
 if __name__ == "__main__":
     path = r"C:\Users\Administrator\Desktop\GTA4"
-    jpg2png(path)
+    png2jpg(path)
